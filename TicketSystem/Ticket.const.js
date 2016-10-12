c075df55e9d805b52b2ce28b0bb71fe133b485b3
Spyingwind <spyingwind@gmail.com>
2016-10-12 18:17:34 -0500

    Create Ticket.const.js

---
+++ TicketSystem/Ticket.const.js
@@ -0,0 +1,29 @@
+const TASK_IDLE = 'Idle';
+const TASK_MINE = 'Mine';
+const TASK_UPGRADE_CONTROLLER = 'UpgradeController';
+const TASK_FILL_STORAGE = 'FillStorage';
+const TASK_FILL_SPAWN = 'FillSpawn';
+const TASK_FILL_TERMINAL = 'FillTerminal';
+const TASK_FILL_TOWER = 'FillTower';
+const TASK_FILL_LAB = 'FillLab';
+const STATUS_OPEN = 'open';
+const STATUS_CLOSED = 'closed';
+const STATUS_WORKING = 'working';
+const STATUS_PENDING = 'pending';
+const STATUS_COMPLETED = 'completed';
+
+module.export = {
+    TASK_IDLE,
+    TASK_MINE,
+    TASK_UPGRADE_CONTROLLER,
+    TASK_FILL_STORAGE,
+    TASK_FILL_SPAWN,
+    TASK_FILL_TERMINAL,
+    TASK_FILL_TOWER,
+    TASK_FILL_LAB,
+    STATUS_OPEN,
+    STATUS_CLOSED,
+    STATUS_WORKING,
+    STATUS_PENDING,
+    STATUS_COMPLETED
+};
