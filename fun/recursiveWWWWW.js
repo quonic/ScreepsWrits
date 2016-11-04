308c82bf0fb103cbc495ae1ec616080057088bc4
Spyingwind <spyingwind@gmail.com>
2016-11-04 18:21:38 -0500

    Create recursiveWWWWW.js

---
+++ fun/recursiveWWWWW.js
@@ -0,0 +1,22 @@
+function who(){
+    console.log("Who?");
+    what();
+}
+function what(){
+    console.log("What?");
+    where();
+}
+function where(){
+    console.log("Where?");
+    when();
+}
+function when(){
+    console.log("Why?");
+    why();
+}
+function why(){
+    console.log("Why?");
+    who();
+}
+
+who();
